3c43ae6d943f3d9cfc99a0d4531ec5fb1b552417
Long Tran <lt@ucsd.edu>
2015-04-15 13:33:00 -0600

    Produces a tree representation of a directory.

--- ls_tree.py
+++ ls_tree.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-def list_files(startpath):
-	for root, dirs, files in os.walk(startpath):
-		level = root.replace(startpath, '').count(os.sep)
+def ls(path):
+	for root, dirs, files in os.walk(path):
+		level = root.replace(path, '').count(os.sep)
 		indent = ' ' * 4 * (level) + '/'
 		dir = os.path.basename(root)
 		if dir != 'WCS' and dir != 'CVS':
@@ -14,7 +14,7 @@ def list_files(startpath):
 	
 def main():
 	if len(sys.argv) == 2:
-		list_files(str(sys.argv[1]))
+		ls(str(sys.argv[1]))
 	else:
 		print 'Usage: python ls_tree.py path'
 
